9daa693a47755c33e323f8d0c8351975e956f19b
Hack Reactor Student <student@hackreactor.com>
2017-12-04 20:54:13 -0800

    add bg and buttons

--- src/init.js
+++ src/init.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
   window.dancers = [];
-
-  $('.addDancerButton').on('click', function(event) {
+  var players = ['Stephen', 'Kobe', 'Durant'];
+  var number = 0;
+  $('#createDancer').on('click', function(event) {
     /* This function sets up the click handlers for the create-dancer
      * buttons on dancefloor.html. You should only need to make one small change to it.
      * As long as the "data-dancer-maker-function-name" attribute of a
@@ -28,7 +29,9 @@ $(document).ready(function() {
       Math.random() * 1000
     );
     $('body').append(dancer.$node);
-    window.dancers.push(dancer);
+    dancer.$node[0].setAttribute('id', players[number]);
+    number ++;
+    window.dancers.push(dancer.$node[0]);
   });
   
 });
